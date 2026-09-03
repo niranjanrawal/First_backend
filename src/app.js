690105e636a2fcf76070d5ea1cbb4e16d3aea1dd
unknown <niranjanrawal466@gmail.com>
2026-09-03 13:19:34 +0530

    disscused and updated utils files

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+
+}))
+
+app.use(express.json({limit : "18kb"}))
+
+app.use(express.urlencoded({extended  : true , limit :"18kb"}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
+
+
+
+const app = express()
+
+
+export{ app }
--- src/index.js
+++ src/index.js
@@ -9,4 +9,15 @@ import connectDB from './db/index.js';
 
 
 
-connectDB();
+connectDB()
+
+.then(() =>{
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`server is running at port ${process.env.PORT}`);
+    });
+})
+
+.catch((err) =>{
+    console.log("mongo db connection failed" , err);
+    
+})
